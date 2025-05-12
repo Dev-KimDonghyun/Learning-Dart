461eefe28833a0f5c34a7080cde6919d63b074ce
Dev-KimDonghyun <hyungus723@gmail.com>
2025-05-12 10:40:23 +0900

    study: Flutter 학습 기본 환경 구축

---
+++ flutter_learn_1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // 여기에 코드 입력
+    );
+  }
+}
